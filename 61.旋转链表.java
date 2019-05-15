56ddc0c93462acceff169dfb582186600d9a6573
TimberTang <ming.tang@ndmicro.com>
2019-05-15 11:46:59 +0800

    61 rotate listNode

--- 61.旋转链表.java
+++ 61.旋转链表.java
@@ -13,7 +13,27 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        
+        if (head == null || k == 0) {
+            return head;
+        }
+        int length = 1;
+        ListNode trail = head;
+        while (trail.next != null) {
+            length ++;
+            trail = trail.next;
+        }
+
+        k = length - (k % length);
+        trail.next = head;
+        while (k > 0) { 
+            trail = trail.next;
+            k --;
+        }
+        head = trail.next;
+        trail.next = null; 
+
+        return head;
+
     }
 }
 
